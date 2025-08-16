f5163b5f81140ce6f3b1c948480a589decbdca6f
s25008 <127134600+s25008@users.noreply.github.com>
2025-08-16 10:38:48 +0200

    Commit

--- S25008/settings.py
+++ S25008/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sr*zanm(809d(rl&m+c%m=ul0l-*8-c*rl^$w&2zbk!+98y+*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['s25008.pythonanywhere.com']
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Application definition
 
